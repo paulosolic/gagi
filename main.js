c1a689abb582f3019db5aef65bc10cecead1eeef
Paulo Šolić <59966561+paulosolic@users.noreply.github.com>
2023-06-21 12:10:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Add an event listener to the invitation element
+const invitation = document.querySelector('.invitation');
+invitation.addEventListener('click', revealDetails);
+
+// Function to reveal the invitation details
+function revealDetails() {
+  // Add your code here to animate the card and reveal the details
+}
